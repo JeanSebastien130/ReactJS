8d5d54b485772be5868b41933af8fec1df38c70a
Jean-Sebastien Morel <js.morel130@gmail.com>
2022-03-02 16:56:13 +0100

    components, useState, Map

--- Project2/react-task-tracker/src/App.js
+++ Project2/react-task-tracker/src/App.js
@@ -1,9 +1,23 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "An appointment", day: "1 janvier", reminder: true },
+    { id: 2, text: "An other appointment", day: "2 janvier", reminder: true },
+    {
+      id: 3,
+      text: "An other other appointment",
+      day: "3 janvier",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- Project2/react-task-tracker/src/components/Header.js
+++ Project2/react-task-tracker/src/components/Header.js
@@ -1,9 +1,27 @@
-const Header = (props) => {
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Hello" onClick={onClick} />
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS IN JS
+// const headingStyle = { color: "red", backgroundColor: "black" };
 
 export default Header;

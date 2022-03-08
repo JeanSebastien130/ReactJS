89e799799814cf5f4c74f2d561264ce876b75d32
Jean-Sebastien Morel <js.morel130@gmail.com>
2022-03-08 17:25:53 +0100

    left side bar done

--- Project3/my_portfolio/src/App.jsx
+++ Project3/my_portfolio/src/App.jsx
@@ -1,20 +1,35 @@
-import Navbar from "./components/Navbar";
+import RightSidebar from "./components/RightSidebar";
+import LeftSidebar from "./components/LeftSidebar";
 import Banner from "./components/Banner";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import ReachMe from "./components/ReachMe";
+import Bluemarble from "./images/bluemarble.jpg";
+import "./App.css";
+import "./Stylesheet/RightSidebar.css";
+import React from "react";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <>
-        <Banner />
-        <AboutMe />
-        <Projects />
-        <ReachMe />
-      </>
-    </>
+    <div
+      className="backgroundimg"
+      style={{ backgroundImage: `url(${Bluemarble})` }}
+    >
+      <div className="grid-one">
+        <LeftSidebar />
+      </div>
+      <div className="grid">
+        <div className="grid-two">
+          <Banner />
+          <AboutMe />
+          <Projects />
+          <ReachMe />
+        </div>
+      </div>
+      <div className="column-three">
+        <RightSidebar />
+      </div>
+    </div>
   );
 }
 
---
+++ Project3/my_portfolio/src/components/RightSidebar.jsx
@@ -0,0 +1,13 @@
+const RightSidebar = () => {
+  return (
+    <>
+      <ul className="right-sidebar-list">
+        <li>About me</li>
+        <li>My projects</li>
+        <li style={{ marginRight: "100px" }}>Reach me</li>
+      </ul>
+    </>
+  );
+};
+
+export default RightSidebar;

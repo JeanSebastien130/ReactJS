00234499cee6bd9bac4cd6e850d9ff6e1a325c6a
Jean-Sebastien Morel <js.morel130@gmail.com>
2022-03-12 12:57:23 +0100

    add image, links

--- Project3/my_portfolio/src/components/Banner.jsx
+++ Project3/my_portfolio/src/components/Banner.jsx
@@ -1,20 +1,24 @@
 import AaronBeach from "../images/AaronBeach.jpg";
-import AaronBridge from "../images/AaronBridge.jpg";
+// import AaronBridge from "../images/AaronBridge.jpg";
+import AaronMountain from "../images/AaronMountain.jpg";
+// import AaronSmog from "../images/AaronSmog.jpg";
 import "../Stylesheet/Banner.css";
 
 const Banner = () => {
   return (
-    <div>
-      <div className="name">
-        <h1>Jean-Sébastien</h1>
-        <h1>Morel</h1>
-      </div>
-      <div className="fullstack">
-        <h3>Full-Stack Developer</h3>
+    <div className="entirecode">
+      <div className="AllDetails">
+        <div className="name">
+          <h1>Jean-Sébastien</h1>
+          <h1>Morel</h1>
+        </div>
+        <div className="fullstack">
+          <h3>Full-Stack Developer</h3>
+        </div>
       </div>
       <img className="beachpic" src={AaronBeach} alt="img" />
-      <div class="vl"></div>
-      <img className="bridgepic" src={AaronBridge} alt="img" />
+      {/* <div class="vl"></div> */}
+      <img className="mountainpic" src={AaronMountain} alt="img" />
     </div>
   );
 };
--- Project3/my_portfolio/src/components/LeftSidebar.jsx
+++ Project3/my_portfolio/src/components/LeftSidebar.jsx
@@ -1,10 +1,27 @@
 import "../Stylesheet/LeftSidebar.css";
+import LinkedIn from "../images/LinkedIn.png";
+import Github from "../images/github.png";
 
 const LeftSidebar = () => {
   return (
     <>
       <ul className="left-sidebar-list">
         <li style={{ marginLeft: "35px" }}>Scroll for more</li>
+        <div className="Links">
+          <li>
+            <a href="https://www.linkedin.com/in/jsmorel/">
+              <img
+                src={LinkedIn}
+                className="Linkedinpic"
+                alt="Linkedinlink"
+              ></img>
+            </a>
+          </li>
+          <a href="https://github.com/JeanSebastien130">
+            <img src={Github} className="Githubpic" alt="GithubLink"></img>
+          </a>
+          <li></li>
+        </div>
       </ul>
     </>
   );

bc23011738cfaf0ca0d924292264462f4651242e
JS Morel <js.morel130@gmail.com>
2022-04-07 11:47:22 +0200

    portfolio first scratch home page done

--- Project3/my_portfolio/src/components/Banner.jsx
+++ Project3/my_portfolio/src/components/Banner.jsx
@@ -1,4 +1,4 @@
-import AaronBeach from "../images/AaronBeach.jpg";
+import AaronBridge from "../images/AaronBridge.jpg";
 // import AaronBridge from "../images/AaronBridge.jpg";
 import AaronMountain from "../images/AaronMountain.jpg";
 // import AaronSmog from "../images/AaronSmog.jpg";
@@ -7,16 +7,18 @@ import "../Stylesheet/Banner.css";
 const Banner = () => {
   return (
     <div className="entirecode">
-      <div className="AllDetails">
-        <div className="name">
-          <h1>Jean-Sébastien</h1>
-          <h1>Morel</h1>
-        </div>
-        <div className="fullstack">
-          <h3>Full-Stack Developer</h3>
+      <div className="AllDetailsandbridge">
+          <img className="bridgepic" src={AaronBridge} alt="img" />
+        <div className="allDetails">
+          <div className="name">
+            <h1>Jean-Sébastien</h1>
+            <h1>Morel</h1>
+          </div>
+          <div className="fullstack">
+            <h3>Full-Stack Developer</h3>
+          </div>
         </div>
       </div>
-      <img className="beachpic" src={AaronBeach} alt="img" />
       {/* <div class="vl"></div> */}
       <img className="mountainpic" src={AaronMountain} alt="img" />
     </div>
--- Project3/my_portfolio/src/components/LeftSidebar.jsx
+++ Project3/my_portfolio/src/components/LeftSidebar.jsx
@@ -1,24 +1,24 @@
 import "../Stylesheet/LeftSidebar.css";
-import LinkedIn from "../images/LinkedIn.png";
+import LinkedIn from "../images/linkedinlogoblack.png";
 import Github from "../images/github.png";
 
 const LeftSidebar = () => {
   return (
     <>
       <ul className="left-sidebar-list">
-        <li style={{ marginLeft: "60px", marginTop: "10px" }}>
+        <li style={{ marginLeft: "68px", marginTop: "10px" }}>
           Scroll for more
         </li>
         {/* <div className="Links"> */}
         <li style={{ marginRight: "20px" }}>
-          <a href="https://www.linkedin.com/in/jsmorel/">
+          <a href="https://www.linkedin.com/in/jsmorel/" target="_blank" rel="noreferrer">
             <img
               src={LinkedIn}
               className="Linkedinpic"
               alt="Linkedinlink"
             ></img>
-            <a href="https://github.com/JeanSebastien130">
-              <img src={Github} className="Githubpic" alt="GithubLink"></img>
+            <a href="https://github.com/JeanSebastien130" target="_blank" rel="noreferrer">
+              <img src={Github} className="Githubpic" alt="GithubLink" ></img>
             </a>
           </a>
         </li>

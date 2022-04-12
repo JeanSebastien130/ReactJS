131b785de0676ddd680fbe236ce825b27ae211f2
JS Morel <js.morel130@gmail.com>
2022-04-12 14:49:43 +0200

    cards works setup

--- Project3/my_portfolio/src/components/MyProjects.jsx
+++ Project3/my_portfolio/src/components/MyProjects.jsx
@@ -1,21 +1,37 @@
 import '../Stylesheet/MyProject.css'
+import '../Stylesheet/card_ft.css'
+import '../Stylesheet/card_abb.css'
 import Fasttrack from '../images/Fasttrack.png'
-// import Artbnb from '../images/Artbnb.png'
+import Artbnb from '../images/Artbnb.png'
 
 const myProjects = () => {
   return (
   <div className='myprojectsbanner'>  
       <h1 className='headermyprojects'>My works</h1>
     <div className='entirecodemyprojects'>
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
+    <div class="flip-card-ft">
+        <div class="flip-card-inner-ft">
+          <div class="flip-card-front-ft">
             <img className='fasttrackpic' src={Fasttrack} alt="image1" />
           </div>
-          <div class="flip-card-back">
-            <h1>John Doe</h1>
-            <p>Architect & Engineer</p>
-            <p>We love that guy</p>
+          <div class="flip-card-back-ft">
+            <div className='flip-card-back-details-ft'>
+              <h1>Fast Track</h1>
+              <p style={{margin: "30px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non porttitor risus. Aenean eu ex et sem interdum auctor. Sed vel lorem ac augue rutrum tempus. Praesent sodales congue nunc, quis tincidunt odio ullamcorper eget.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flip-card-abb">
+        <div class="flip-card-inner-abb">
+          <div class="flip-card-front-abb">
+            <img className='fasttrackpic' src={Artbnb} alt="image1" />
+          </div>
+          <div class="flip-card-back-abb">
+            <div className='flip-card-back-details-abb'>
+              <h1>Artbnb</h1>
+              <p style={{margin: "30px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non porttitor risus. Aenean eu ex et sem interdum auctor. Sed vel lorem ac augue rutrum tempus. Praesent sodales congue nunc, quis tincidunt odio ullamcorper eget.</p>
+            </div>
           </div>
         </div>
       </div>

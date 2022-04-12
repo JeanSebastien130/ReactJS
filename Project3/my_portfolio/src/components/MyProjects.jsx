9233e3d86c7debdad9eb9887a58a695afe5cdef8
JS Morel <js.morel130@gmail.com>
2022-04-12 12:35:52 +0200

    cards works setup

--- Project3/my_portfolio/src/components/MyProjects.jsx
+++ Project3/my_portfolio/src/components/MyProjects.jsx
@@ -1,15 +1,23 @@
 import '../Stylesheet/MyProject.css'
 import Fasttrack from '../images/Fasttrack.png'
-import Artbnb from '../images/Artbnb.png'
+// import Artbnb from '../images/Artbnb.png'
 
 const myProjects = () => {
   return (
   <div className='myprojectsbanner'>  
+      <h1 className='headermyprojects'>My works</h1>
     <div className='entirecodemyprojects'>
-      <h1>My works</h1>
-      <div className="theworks">
-        <img className='fasttrackpic' src={Fasttrack} alt="image1" />
-        <img className='artbnbpic' src={Artbnb} alt="image2" />
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img className='fasttrackpic' src={Fasttrack} alt="image1" />
+          </div>
+          <div class="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>

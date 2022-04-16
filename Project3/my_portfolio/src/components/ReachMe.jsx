38329afcdffe40c0eeba58329ae0333866fca9e2
JS Morel <js.morel130@gmail.com>
2022-04-16 15:21:02 +0200

    setup reach me form

--- Project3/my_portfolio/src/components/ReachMe.jsx
+++ Project3/my_portfolio/src/components/ReachMe.jsx
@@ -4,14 +4,24 @@ const ReachMe = () => {
   return (
   <div className="reachmeentirecode">
     <div className='thewholeform'>
-      <label for="firstName">First name</label>
-      <input type="text" name="firstName" className="firstName" tabIndex="1"/>
-      <label for="lastName">Last name</label>
-      <input type="text" name="lastName" className="lastName" tabIndex="2"/>
-      <label for="lastName">Your mail address</label>
-      <input type="text" name="lastName" className="lastName" tabIndex="2"/>
-      <label for="lastName">Your message</label>
-      <input type="text" name="lastName" className="lastName" tabIndex="2"/>
+      <div className='details'>
+        <label for="firstName">First name</label>
+        <input type="text" name="firstName" className="firstName" tabIndex="1"/>
+        <label for="lastName">Last name</label>
+        <input type="text" name="lastName" className="lastName" tabIndex="2"/>
+      </div>
+      <div className='mailadress'>
+        <label for="Mail adress">Your mail address</label>
+        <input type="text" name="mail" className="lastName" tabIndex="2"/>
+      </div>
+      <div className='subject'>
+        <label for="Subject">Subject</label>
+        <input type="text" name="Subject" className="lastName" tabIndex="2"/>
+      </div>
+      <div className='message'>
+        <label for="Your message">Your message</label>
+        <input type="text" name="lastName" className="lastName" tabIndex="2"/>
+      </div>
       <button type='submit'>Submit</button>
     </div>
   </div>
